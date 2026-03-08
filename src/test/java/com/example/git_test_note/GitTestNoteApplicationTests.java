7ae3994dcfb6da2ef1615b6118b3c8c861905d3d
Merikh002 <rakotondrainytahiana@gmail.com>
2026-03-08 22:52:25 +0300

    initialisation du projet aloha

---
+++ src/test/java/com/example/git_test_note/GitTestNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_test_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
